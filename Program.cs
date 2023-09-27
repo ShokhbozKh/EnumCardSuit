a96861108676e0251323f929b2281831018a3cfe
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-27 09:31:40 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Karta kartalar in Enum.GetValues(typeof(Karta)))
+            {
+                Console.WriteLine(kartalar);
+            }
+        }
+    }
+    enum Karta
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+}
